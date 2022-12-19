a1740199523dc80f0bfa3a40d7447eec8f169fcf
Rafael <rrussoporchera@gmail.com>
2022-12-18 22:08:41 -0300

    cancel-notification

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,13 +1,13 @@
-import { SendNotification } from './send-notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
 
-describe('Send Notification', () => {
+describe('Send notification', () => {
   it('should be able to send a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
-      content: 'this is a notification',
+      content: 'This is a notification',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
